1d8176767abaccd5467b19756a0d69765b2802b4
mylamour <mylamour@163.com>
2018-06-04 19:00:45 +0800

    changed config file

--- terraform/tutorial/multi/coin/karbo/module.tf
+++ terraform/tutorial/multi/coin/karbo/module.tf
@@ -32,15 +32,28 @@ data "aws_ami" "default" {
   owners = ["amazon"]
 }
 
+resource "aws_key_pair" "local" {
+  key_name   = "local"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDyHfIBLpGWzmOi35aSn7mjib+LN1GHKraWlas6aNW7lqB1QC0H2SnyDX3t9gPBbQ4ilnZWUf2/NK9DbxsnmQYb/XBEP0S5NfpALVYLgTudi5cBoQP8b9Z1/O9yUuIu0MTgETYYBrRT5mHVk0yMWiQ625RCjEQ1hDhVYQS5yO4k4T0yQYjZeGZmXZGL2J84+H5Hj+Og3cfWh3ndpqz93n7EAWHnIjNOKoyof8l59McKq36+K2cAJwE1r/ObZzM/vAT01q89Cqm4gMD+sB2/+GhqRmOaD/NmUYkBlQ6Sn8Cz0y6c1s6McUVZwUpIbbYKBgM6RVL/pT6w90lzfThX783 mour@lime.local"
+}
+
 resource "aws_instance" "karbo" {
   ami                = "${data.aws_ami.default.id}"
   instance_type      = "t2.micro"
+  key_name = "${aws_key_pair.local.key_name}"
+  
   tags {
     Name = "coinnode-karbo"
   }
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.karbo.public_ip} >> ip_address.txt"
+  }
+  
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
   }
 
   provisioner "file" {
@@ -53,12 +66,4 @@ resource "aws_instance" "karbo" {
       "sudo bash /tmp/install_karbo.sh"
     ]
   }
-  timeout {
-    
-  }
-}
-
-resource "aws_key_pair" "local" {
-  key_name   = "local"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDyHfIBLpGWzmOi35aSn7mjib+LN1GHKraWlas6aNW7lqB1QC0H2SnyDX3t9gPBbQ4ilnZWUf2/NK9DbxsnmQYb/XBEP0S5NfpALVYLgTudi5cBoQP8b9Z1/O9yUuIu0MTgETYYBrRT5mHVk0yMWiQ625RCjEQ1hDhVYQS5yO4k4T0yQYjZeGZmXZGL2J84+H5Hj+Og3cfWh3ndpqz93n7EAWHnIjNOKoyof8l59McKq36+K2cAJwE1r/ObZzM/vAT01q89Cqm4gMD+sB2/+GhqRmOaD/NmUYkBlQ6Sn8Cz0y6c1s6McUVZwUpIbbYKBgM6RVL/pT6w90lzfThX783 mour@lime.local"
 }
